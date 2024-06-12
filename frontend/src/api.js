81956c68ecc24f6b2a387d2522f764bad90716cd
ardaaltinorsardaaltinors@gmail.com <ardaaltinors@gmail.com>
2024-06-12 22:09:49 +0300

    chore: Update Docker configuration

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -5,8 +5,6 @@ const api = axios.create({
 	baseURL: import.meta.env.VITE_API_URL,
 });
 
-console.log(api.defaults.baseURL);
-
 api.interceptors.request.use(
 	(config) => {
 		const token = localStorage.getItem(ACCESS_TOKEN);
